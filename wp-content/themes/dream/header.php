591e20955e7eb3564ed75ed5ff9003091ba059ba
Greg Nemes <mail@gregnemes.com>
2014-07-18 15:39:59 -0400

    Header progress

--- wp-content/themes/dream/header.php
+++ wp-content/themes/dream/header.php
@@ -29,7 +29,7 @@
 				   
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="google-site-verification" content="">
-	<meta name="author" content="Greg Nemes">
+	<meta name="author" content="Work-Shop">
 		
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	
@@ -42,13 +42,31 @@
     <![endif]-->		
     	
 	<?php wp_head(); ?>
-	
-	<script src="<?php bloginfo('template_directory'); ?>/_/js/less.js"></script>
-		
+			
 </head>
 
-<body <?php body_class('before block-closed'); ?>>
+<body <?php body_class('before menu-closed'); ?>>
 
-	<div id="site-background"></div>
+	<header id="header">
+	
+		<div class="container">
+		
+			<div class="row header-top">
+				<div class="left">
+					<h3 class="page-title"><?php the_title(); ?></h3>
+				</div>
+				<a id="nav-toggle" href="#" class="right">
+					menu<span class="icon" data-icon="x"></span>
+				</a>
+			</div>
+			
+			<nav class="row header-nav">
+				<div id="blanket"></div>
+				<?php get_template_part('engine-nav'); ?>			
+			</nav>
+			
+		</div>		
+
+	</header>
 		
 	<div id="content">
