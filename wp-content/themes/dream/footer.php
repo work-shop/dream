930f5d9e8321a4f769af95ee6e60cb2efca2420e
Nic Schumann <nicolas_schumann@brown.edu>
2014-08-07 15:47:51 -0400

    minimal script updates

--- wp-content/themes/dream/footer.php
+++ wp-content/themes/dream/footer.php
@@ -24,11 +24,17 @@
     		if ( $file_name = get_field('script_file') ) {
 
     			echo '<script type="text/javascript" charset="utf-8" lang="javascript" src="'
+    			     .get_template_directory_uri().'/_/js/preprocessor.js"></script>';
+
+    			echo '<script type="text/javascript" charset="utf-8" lang="javascript" src="'
     			     . get_template_directory_uri() . '/' . 'script_files/' . $file_name . '"></script>';
 
     		} else if ( $interaction = get_field('interaction') 
     				    && $file_name = get_field( 'script_file', $interaction->ID ) ) {
 
+    			echo '<script type="text/javascript" charset="utf-8" lang="javascript" src="'
+    			     .get_template_directory_uri().'/_/js/preprocessor.js"></script>';
+    			     
     			echo '<script type="text/javascript" charset="utf-8" lang="javascript" src="'
     			     . get_template_directory_uri() . '/' . 'script_files/' . $file_name . '"></script>';
 
