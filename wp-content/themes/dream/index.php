6d6d9c0acc01f7123d53cf1f6642026483336f16
Greg Nemes <mail@gregnemes.com>
2014-08-22 14:35:31 -0400

    New database with fields

--- wp-content/themes/dream/index.php
+++ wp-content/themes/dream/index.php
@@ -4,27 +4,27 @@
 <section id="home" class="block crop">
 	<div id="dreams">
 	
-	<article class="dream">
+	<article class="dream" width="" height="" top="" left="">
 
 		<div class="drawing">
-			<img src="" />
+			<img src="http://localhost:8888/dream/wp-content/uploads/2014/08/test.jpg" alt="drawing1" />
 		</div>
 
 		<div class="info">
 
 			<hgroup class="title">
-				<h2 class="dream-title"></h2>
-				<h3 class="dream-number"></h3>
+				<h2 class="dream-title">Fennel</h2>
+				<h3 class="dream-number">Dream No. 731,456</h3>
 			</hgroup>
 
 			<hgroup class="metadata">
-				<h4 class="dream-length"></h4>
-				<h4 class="dream-author"></h4>
-				<h4 class="dream-date"></h4>
-				<h4 class="dream-drawing-title"></h4>
+				<h4 class="dream-length">342 words</h4>
+				<h4 class="dream-author">Written by Crystal Gandrud</h4>
+				<h4 class="dream-date">12.24.13</h4>
+				<h4 class="dream-drawing-title">Drawing No. 148: Anonymous</h4>
 			</hgroup>
 
-			<h5 class="excerpt"></h5>
+			<h5 class="excerpt">I eat a fennel salad. I become aware that the Venerable Chogyam Trungpa Rinpoche is watching me eat.</h5>
 			
 		</div>
 
