0869ed6d183f4246b6c7133d03242d8368c9d245
Nic Schumann <nicolas_schumann@brown.edu>
2014-08-22 14:13:01 -0400

    added dream structure on index

--- wp-content/themes/dream/index.php
+++ wp-content/themes/dream/index.php
@@ -1,5 +1,38 @@
 
 <?php get_header();?>
 
+<section id="home" class="block crop">
+	<div id="dreams">
+	
+	<article class="dream">
+
+		<div class="drawing">
+			<img src="" />
+		</div>
+
+		<div class="info">
+
+			<hgroup class="title">
+				<h2 class="dream-title"></h2>
+				<h3 class="dream-number"></h3>
+			</hgroup>
+
+			<hgroup class="metadata">
+				<h4 class="dream-length"></h4>
+				<h4 class="dream-author"></h4>
+				<h4 class="dream-date"></h4>
+				<h4 class="dream-drawing-title"></h4>
+			</hgroup>
+
+			<h5 class="excerpt"></h5>
+			
+		</div>
+
+	</article>
+
+
+	</div>
+</section>
+
 		
 <?php get_footer(); ?>
