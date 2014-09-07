8369364f0f54d4d396ebf94f255a99a542adf7e2
Greg Nemes <mail@gregnemes.com>
2014-09-06 23:11:31 -0400

    Front matter layout

--- wp-content/themes/dream/index.php
+++ wp-content/themes/dream/index.php
@@ -91,6 +91,12 @@
 				<?php if (has_post_thumbnail()) : ?> 	
 				<div class="dream-drawing" style="<?php echo arr_format_size( $fixed_vals[ $i % count( $fixed_vals ) ] ); ?>">
 					<?php the_post_thumbnail('drawing') ?>
+					<div class="drawing-info">
+						<?php if ( $dream_number ) : ?><h3 class="dream-number">Dream No. <?php echo $dream_number; ?></h3><?php endif; ?>
+						<h2 class="dream-title"><?php the_title(); ?></h2>
+						<h3 class="excerpt">I eat a fennel salad. I become aware that the Venerable Chogyam Trungpa Rinpoche is watching me eat.</h3>						
+					</div>
+
 				</div>
 				<?php endif; ?>
 				
@@ -99,19 +105,23 @@
 					<div class="dream-info">
 			
 						<hgroup class="title">
-							<?php if ( $dream_number ) : ?><h3 class="dream-number">Dream No. <?php echo $dream_number; ?></h3><?php endif; ?>
-							<h2 class="dream-title"><?php the_title(); ?></h2>
-						</hgroup>
-			
+							<?php if ( $dream_number ) : ?>
+							<h4 class="dream-number">Dream No. <?php echo $dream_number; ?></h4>
+							<?php endif; ?>
+										
 						<hgroup class="metadata">
-							<?php if ( $dream_length ) : ?><h4 class="dream-length"><?php echo $dream_length; ?> words</h4><?php endif; ?>
-							<?php if ( $dream_author ) : ?><h4 class="dream-author">Written by <?php echo $dream_author; ?></h4><?php endif; ?>
-							<?php if ( $dream_date ) : ?><h4 class="dream-date"><?php echo $dream_date; ?></h4><?php endif; ?>
+							<?php if ( $dream_length ) : ?><h4 class="dream-length"><?php echo $dream_length; ?> words &nbsp;&nbsp;&nbsp;&nbsp;<?php endif; ?>
+							<?php if ( $dream_author ) : ?>Written by <?php echo $dream_author; ?> &nbsp;&nbsp;&nbsp;&nbsp;<?php endif; ?>
+							<?php if ( $dream_date ) : ?><?php echo $dream_date; ?><?php endif; ?>
 							<?php if ( $dream_drawing_title ) : ?><h4 class="dream-drawing-title">Drawing: <?php echo $dream_drawing_title; ?></h4><?php endif; ?>
 						</hgroup>
-			
-						<?php // if ( get_the_field('dream_excerpt', $id ) ) : ?><h3 class="excerpt">I eat a fennel salad. I become aware that the Venerable Chogyam Trungpa Rinpoche is watching me eat.<?php // the_field( 'dream_excerpt', $id ); ?></h3><?php // endif; ?>
 						
+							<h2 class="dream-title"><?php the_title(); ?></h2>
+						</hgroup>
+
+						
+						<h4 class="excerpt italic">Chemistry is a sort of a dream. Or a code anyway.</h4>						
+									
 					</div>
 				
 					<div class="dream-text">
@@ -119,7 +129,7 @@
 						<div class="container">
 							<div class="row">			
 						
-								<div class="dream-text-inner serif italic ">
+								<div class="dream-text-inner serif ">
 									<?php the_field('dream_text'); ?>
 								</div>
 								
